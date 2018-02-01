5b425fe493c43d0682692c9625e5402bd5a3bbc5
vmarvaso <36050006+vmarvaso@users.noreply.github.com>
2018-02-01 12:25:02 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./urislickson-6245332cc9d5.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+  databaseURL: "https://urislickson-d2cef.firebaseio.com/"
 });
 
 // Setup
